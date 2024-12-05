fafb3d72c0f87d60d3759196076703ca0c1e52c4
KR-HS <jjc5989@naver.com>
2024-12-05 17:44:32 +0900

    2일차

--- 코딩 연습/Java/Main.java
+++ 코딩 연습/Java/Main.java
@@ -5,24 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-
+        String[] st = sc.nextLine().split(" ");
+        int a1=int(st[0]);
         
-        int maxNum=Math.max(a,Math.max(b,c));
-
-        int low_sum=a+b+c-maxNum;
-
-        if(low_sum>maxNum){
-            System.out.println(low_sum+maxNum);
-        }
-        else if(low_sum==maxNum){
-            System.out.println(low_sum+maxNum-1);
-        }
-        else{
-            System.out.println(low_sum+low_sum-1);
-        }
+        int c=sc.nextInt();
+        int n0=sc.nextInt();
         
         
 
