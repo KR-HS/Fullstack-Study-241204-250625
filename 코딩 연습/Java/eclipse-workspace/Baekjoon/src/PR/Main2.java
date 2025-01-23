a0da2d815b34e440c97b02905603b4079bcb8c7a
KR-HS <jjc5989@naver.com>
2025-01-23 09:16:28 +0900

    26일차 수정

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
@@ -3,26 +3,20 @@ import java.util.*;
 import java.io.*;
 public class Main2 {
 
-	public static void main(String args[]) throws NumberFormatException,IOException{
-		Scanner sc = new Scanner(System.in);
-        int minX=Integer.MAX_VALUE;
-        int minY=Integer.MAX_VALUE;
-        int maxX=0;
-        int maxY=0;
-        int count=sc.nextInt();
-        if(count==1){
-            System.out.println(0);
-            return;
-        }
-        for(int i=0;i<count;i++){
-            int x= sc.nextInt();
-            int y = sc.nextInt();
-            if(minX>x) minX=x;
-            if(minY>y) minY=y;
-            if(maxX<x) maxX=x;
-            if(maxY<y) maxY=y;
-        }
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+        String[] str = sc.nextLine().split("");
+        int[] arr = new int[str.length];
         
-        System.out.println((maxX-minX)*(maxY-minY));
+        for(int i=0;i<arr.length;i++){
+            arr[i]=Integer.parseInt(str[i]);
+        }
+        Arrays.sort(arr);
+        
+        for(int i=arr.length-1;i>=0;i--){
+           System.out.print(arr[i]); 
+        }
+        System.out.println();
+        sc.close();
     }
 }
