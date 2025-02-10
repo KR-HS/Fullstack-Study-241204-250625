b16c80a1e591c1bef3942cf3701c16d1a37ca5ca
KR-HS <jjc5989@naver.com>
2025-02-10 10:53:56 +0900

    34일차 중간저장

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
@@ -1,32 +1,39 @@
 package PR;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Stack;
 public class Main2 {
 	public static void main(String args[]) throws IOException,NumberFormatException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
+		int n=Integer.parseInt(br.readLine());
+		Stack<Integer> s = new Stack<>();
 		for(int i=0;i<n;i++) {
-			long input = Long.parseLong(br.readLine());
-			if(input==0||input==1) {
-				bw.write(2+"\n"); 
-				continue;
+			String input=br.readLine();
+			if(input.length()>1) {
+				String[] str= input.split(" ");
+				int command = Integer.parseInt(str[0]);
+				int num = Integer.parseInt(str[1]);
+				if(command==1) s.push(num);
+			}else {
+				int command = Integer.parseInt(input);
+				if(command==2) {
+					if(!s.isEmpty()) bw.write(s.pop()+"\n");
+					else bw.write(-1+"\n");
+				}else if(command==3) bw.write(s.size()+"\n");
+				else if(command==4) {
+					if(s.isEmpty()) bw.write(1+"\n");
+					else bw.write(0+"\n");
+				}else if(command ==5) {
+					if(!s.isEmpty()) bw.write(s.peek()+"\n");
+					else bw.write(-1+"\n");
+				}
 			}
 			
-			find:while(true) {
-				for(int j=2;j<=Math.sqrt(input);j++) {
-					if(input%j==0) {
-						input++;
-						continue find;
-					}
-				}
-				System.out.println(input);
-				break;
-			}
-			bw.write(input+"\n");
 		}
+
 		bw.flush();
 		bw.close();
 		br.close();
 	}
-
 }
