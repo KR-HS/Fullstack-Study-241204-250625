799407d3f5a942fee3de692b43fd4fbb832137d3
KR-HS <jjc5989@naver.com>
2025-02-14 17:00:42 +0900

    38일차

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
@@ -9,24 +9,60 @@ public class Main2 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		
-        int n= Integer.parseInt(br.readLine());
+        int tc= Integer.parseInt(br.readLine());
         
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++) {
-        	arr[i]=Integer.parseInt(br.readLine());
+        
+        
+        for(int i=0;i<tc;i++) {
+        	// qu에 넣을 개수, 찾을 위치
+        	String[] str = br.readLine().split(" ");
+        	int n = Integer.parseInt(str[0]);
+        	int pos = Integer.parseInt(str[1]);
+        	
+        	
+        	// qu에 넣을 데이터
+        	String[] arr = br.readLine().split(" ");
+        	
+        	// 찾는 위치의 중요도
+        	int target=Integer.parseInt(arr[pos]);
+
+        	// 타겟보다 큰 개수
+        	int count=0;
+
+        	Queue<Integer> qu = new LinkedList<>();
+        	for(String a: arr) {
+        		qu.offer(Integer.parseInt(a));
+        	}
+        	
+        	// 타겟의 위치 이하의 같은값 개수
+        	int under = 0;
+        	
+        	// 타켓위치 바로 뒤의 큰값 뒤에 오는 타겟과 같은 수 카운팅
+        	int upper = 0;
+        	boolean isBig=false;
+
+        	for(int j=0;j<n;j++) {
+        		int poll= qu.poll();
+        		if(j<=pos) {
+        			if(poll==target) under++;
+        			if(poll>target) count++;
+        		}else {
+        			if(poll>target) {
+            			count++;
+            			isBig=true;
+            		}
+                    if(poll==target&&isBig) upper++;
+        			
+        			
+        		}
+        	}
+        	bw.write("count:"+count+",under:"+under+",upper:"+upper+"\n");
+
+        	bw.write(count+under+upper+"\n");
+        	
         }
-        Arrays.sort(arr);
+
         
-        int cut=(int)Math.round(arr.length*0.15);
-        long sum=0;
-        
-        int count = n-cut*2;
-        for(int i=cut;i<n-cut;i++) {
-        	sum+=arr[i];
-        }
-        
-        
-        bw.write((int)Math.round(sum/(count*1.0))+"\n");
 		bw.flush();
 		bw.close();
 		br.close();	
