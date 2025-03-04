60bdc81ad22c923b76bb3391ecf12b82f27923f1
KR-HS <jjc5989@naver.com>
2025-03-04 17:20:13 +0900

    49일차

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
@@ -8,69 +8,26 @@ public class Main2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
-        Map<String,Integer> map = new HashMap<>();
+        int n = Integer.parseInt(str[0]);	
+        int m = Integer.parseInt(str[1]);	
+        int arr[] = new int[n+1];
+        int dp[] = new int[n+1];
+        String[] str1 =br.readLine().split(" ");
+        for(int i=1;i<n+1;i++) {
+        	arr[i]=Integer.parseInt(str1[i-1]);
+        	dp[i]=dp[i-1]+arr[i];
+        }
+
+        System.out.println(Arrays.toString(dp));
+		while(m>0) {
+			m--;
+			String[] str2 = br.readLine().split(" ");
+			int start=Integer.parseInt(str2[0]);
+			int end=Integer.parseInt(str2[1]);
+			
+			bw.write(dp[end]-dp[start-1]+"\n");
+		}
         
-        
-         for(int i=0;i<n;i++) {
-        	String input = br.readLine();
-        	if(input.length()<m) continue;
-        	if(map.containsKey(input)) {
-        		map.put(input, map.get(input)+1);
-        	}else {
-        		map.put(input, 1);
-        	}
-        }    
-//         List<String> list = new ArrayList<>();
-//         for(String key:map.keySet()) {
-//         	if(list.size()==0) list.add(key);
-//         	else {
-//         		for(int i=0;i<list.size();i++) {
-//         			if(map.get(key)>map.get(list.get(i))) {
-//         				list.add(i,key);
-//         				break;
-//         			}else if(map.get(key)==map.get(list.get(i))) {
-//         				if(key.length()>list.get(i).length()) {
-//         					list.add(i,key);
-//         					break;
-//         				}else if(key.length()==list.get(i).length()) {
-//         					if(key.compareTo(list.get(i))<0) {
-//         						list.add(i,key);
-//         						break;
-//         					}
-//         				}else {
-//         					if(i==list.size()-1) {
-//             					list.add(key);
-//             					break;
-//             				}
-//         				}
-//         			}else {
-//         				if(i==list.size()-1) {
-//         					list.add(key);
-//         					break;
-//         				}
-//         			}
-//         		}
-//         	}
-//         }
-         
-         List<String> list = new ArrayList<>(map.keySet());
-         list.sort((a, b) -> {
-             int countComparison = Integer.compare(map.get(b), map.get(a)); // 빈도수 내림차순
-             if (countComparison != 0) return countComparison;
-             
-             // 빈도수가 같으면 길이 내림차순
-             int lengthComparison = Integer.compare(b.length(), a.length());
-             if (lengthComparison != 0) return lengthComparison;
-             
-             // 길이도 같으면 사전순 오름차순
-             return a.compareTo(b);
-         });
-         
-         for(String s: list) {
-         	bw.write(s+"\n");
-         }
         bw.flush();
         bw.close();
         br.close();
