29011dc0d2a6d793bf3c651b6520999ddf08af0f
KR-HS <jjc5989@naver.com>
2025-03-05 17:14:36 +0900

    50일차

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
@@ -3,31 +3,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main2 {	
+	static int[] dp = new int[50001];
 	
+	public static void getDp(int n) {
+		for(int i=1;i<=n;i++) {
+			if(dp[i]!=0) continue;
+			int N =(int)Math.sqrt(i);
+			
+			int count=5;
+			if(i==(int)(Math.pow(N,2))) { 
+				dp[i]=1;
+			}else {
+				while(N>0) {
+					if(count>dp[(int)(Math.pow(N,2))]+dp[i-(int)(Math.pow(N,2))]) {
+						dp[i]=dp[(int)(Math.pow(N,2))]+dp[i-(int)(Math.pow(N,2))];
+						count=dp[i];
+					}
+					N--;
+				}
+			}
+		}
+	}
 	public static void main(String args[]) throws IOException,NumberFormatException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);	
-        int m = Integer.parseInt(str[1]);	
-        int arr[] = new int[n+1];
-        int dp[] = new int[n+1];
-        String[] str1 =br.readLine().split(" ");
-        for(int i=1;i<n+1;i++) {
-        	arr[i]=Integer.parseInt(str1[i-1]);
-        	dp[i]=dp[i-1]+arr[i];
-        }
-
-        System.out.println(Arrays.toString(dp));
-		while(m>0) {
-			m--;
-			String[] str2 = br.readLine().split(" ");
-			int start=Integer.parseInt(str2[0]);
-			int end=Integer.parseInt(str2[1]);
-			
-			bw.write(dp[end]-dp[start-1]+"\n");
-		}
+        int n = Integer.parseInt(br.readLine());
+        getDp(n);
         
+        bw.write(dp[n]+"\n");
         bw.flush();
         bw.close();
         br.close();
