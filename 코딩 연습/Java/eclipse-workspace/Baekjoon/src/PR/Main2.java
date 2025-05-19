58d8dbd00ee93ff4e7e0ccbe99e751195f95bf80
KR-HS <jjc5989@naver.com>
2025-05-19 17:06:16 +0900

    108일차

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main2.java
@@ -5,26 +5,19 @@ import java.util.*;
 public class Main2 {	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	public static void hanoi(int n, int from, int to, int aux) throws IOException,NumberFormatException{
-        // 원판이 하나일 경우, 바로 이동
-        if (n == 1) {
-            bw.write(from + " " + to+"\n");
-            bw.flush();
-            return;
-        }
-        
-        // 1. n-1개의 원판을 from -> aux로 이동
-        hanoi(n - 1, from, aux, to);
-        
-        // 2. 가장 큰 원판을 from -> to로 이동
-        bw.write(from + " " + to+"\n");
-        
-        // 3. n-1개의 원판을 aux -> to로 이동
-        hanoi(n - 1, aux, to, from);
+//	static int count=0;
+	static int n;
+	public static void hanoi(int n, int from, int to, int temp) throws IOException,NumberFormatException{
+       if(n>0) {
+    	   hanoi(n-1,from,temp,to);
+    	   bw.write(from+" "+to+"\n");
+//    	   count++;
+    	   hanoi(n-1,temp,to,from);
+       }
     }
 	public static void main(String args[]) throws IOException,NumberFormatException{
-        int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
+        
         int totalMoves = (int) Math.pow(2, n) - 1; // 최소 이동 횟수 계산
         
         bw.write(totalMoves+"\n"); // 총 이동 횟수 출력
@@ -32,7 +25,9 @@ public class Main2 {
         // 하노이의 탑 문제 해결
         hanoi(n, 1, 3, 2);
         
-        
+        bw.flush();
+        bw.close();
+        br.close();
         
         
 	}
