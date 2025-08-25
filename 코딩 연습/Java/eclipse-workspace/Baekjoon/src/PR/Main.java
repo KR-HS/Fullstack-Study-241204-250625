f1ea30b3ea9d0af572c149f1f93f4427df3d5b38
KR-HS <jjc5989@naver.com>
2025-08-25 17:39:52 +0900

    포트폴리오 추가

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main.java
@@ -1,62 +1,102 @@
 package PR;
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 public class Main {
-	 
-	public static int[][] BuildMap(BufferedReader br, int n, int m) throws IOException{
-		int[][] map = new int[n][m];
-			 
-		for(int i=0;i<n;i++) {
-			 String line = br.readLine();
-			 map[i] = line.chars().map(c -> c-'0').toArray();
-		 }
-			 
-		return map;
-	}
+
+	static List<Integer>[] list;
 	
-	
-	public static int Solve(int[][] map){
-		int n = map.length;
-		int m = map[0].length;
+	static void bfs(BufferedWriter bw, int n,int v) throws IOException{
+		Queue<Integer> qu = new LinkedList<>();
+		boolean[] checked = new boolean[n+1];
+
 		
-		int[][] visited = new int[n][m];
+		qu.add(v);
+		checked[v]= true;
 		
-		Queue<int[]> qu = new LinkedList<>();
-		int[] dx = {-1,0,1,0};
-		int[] dy = {0,1,0,-1};
-		 
-		qu.add(new int[] {0,0});
-		visited[0][0] = 1;
 		
 		while(!qu.isEmpty()) {
-			 int[] q = qu.poll();
-			 int x = q[0];
-			 int y = q[1];
-			 for(int j=0;j<4;j++) {
-				 int nx = x+dx[j];
-				 int ny = y+dy[j];				 
-				 if(nx<0 || nx >= n || ny < 0 || ny >= m || map[nx][ny]==0 || visited[nx][ny]!=0) continue;
-				 qu.add(new int[] {nx,ny});
-				 visited[nx][ny]=visited[x][y]+1;
-				 
-			 }
-		 }
-		
-		
-		return visited[n-1][m-1]; 
+			int target = qu.poll();
+			bw.write(target+" ");
+			
+			
+			for(int li : list[target]) { 
+				if(!checked[li]) {
+					qu.add(li);
+					checked[li] = true;
+				}
+			}
+			
+		}
+		bw.write("\n");
 	}
+	
+	static void dfs(BufferedWriter bw, int n,int v) throws IOException{
+		boolean[] checked = new boolean[n+1];
+		Stack<Integer> stack = new Stack<>();
+		stack.push(v);
+		
+		while(!stack.isEmpty()) {
+			int pop = stack.pop();
+			if(checked[pop]) continue;
+			checked[pop] = true;
+			bw.write(pop+" ");
+
+			
+			for(int i=list[pop].size()-1;i>=0;i--) {
+				if(!checked[list[pop].get(i)]) stack.push(list[pop].get(i));
+			}
+
+		}
+	
+		bw.write("\n");
+
+	}
+	
+	
+	static int solve(BufferedWriter bw, BufferedReader br, int n, int m,int v) throws IOException{
+		list = new ArrayList[n+1];
+		
+		// 배열 초기화
+		for(int i=1;i<=n;i++) list[i] = new ArrayList<>();
+		
+		
+		// 값 넣기
+		for(int i=0;i<m;i++) {
+			String[] input = br.readLine().split(" ");
+			int in1 = Integer.parseInt(input[0]);
+			int in2 = Integer.parseInt(input[1]);
+			
+			list[in1].add(in2);
+			list[in2].add(in1);
+			
+		}
+		
+		// 정렬
+		for(int i=1;i<=n;i++) {
+			Collections.sort(list[i]);
+		}
+		
+
+		
+		dfs(bw,n,v);
+		bfs(bw,n,v);
+		
+		return 0;
+		
+	}
+	
 	
 	 public static void main(String args[]) throws IOException{
 		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		 int[] nm = Arrays.stream(br.readLine().split(" ")).
-				 mapToInt(Integer::parseInt).
-				 toArray();
-		 int[][] map = BuildMap(br,nm[0],nm[1]);
+		  
+		 int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		 
 		 
+		 
+		 solve(bw, br,nm[0],nm[1],nm[2]);
 		 br.close();
-		 bw.write(Solve(map)+"\n");
 		 bw.flush();
 		 bw.close();
 	 }
