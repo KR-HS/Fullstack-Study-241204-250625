4fb4d2cb64e7d9526f9a4677a8bdb38dbdd95375
KR-HS <jjc5989@naver.com>
2025-08-25 14:10:04 +0900

    포트폴리오 추가

--- 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main.java
+++ 코딩 연습/Java/eclipse-workspace/Baekjoon/src/PR/Main.java
@@ -1,69 +1,63 @@
 package PR;
+import java.io.*;
 import java.util.*;
 public class Main {
-	 public int[] solution(String today, String[] terms, String[] privacies) {
-	        List<Integer> answer = new ArrayList<>();
-	        // 오늘 날짜
-	        String[] today_date=today.split(".");
-	        int today_year=Integer.parseInt(today_date[0]);
-	        int today_month=Integer.parseInt(today_date[1]);
-	        int today_day=Integer.parseInt(today_date[2]);
-	        
-	        //계약 
-	        int count=1;
-	        for(String privacy:privacies){
-	            String[] pr=privacy.split(" ");
-	            // 계약 종류
-	            String pr_var=pr[1];
-	            
-	            //계약당시 날짜
-	            String[] pr_date=pr[0].split(".");
-	            int pr_year=Integer.parseInt(pr_date[0]);
-	            int pr_month=Integer.parseInt(pr_date[1]);
-	            int pr_day=Integer.parseInt(pr_date[2]);
-	            
-	            for(String term:terms){
-	                String[] t=term.split(" ");
-	                if(pr_var.equals(t[0])){
-	                    pr_month+=Integer.parseInt(t[1]);
-	                    break;
-	                }
-	            }
-	            if(pr_month>12){
-	                if(pr_month%12==0){
-	                    pr_year+=(pr_month/12-1);
-	                    pr_month=12;
-	                }
-	                else{
-	                    pr_year+=pr_month/12;
-	                    pr_month%=12;
-	                }
-	            }
-	            if(today_year<pr_year){
-	                answer.add(count);
-	            }
-	            else if(today_year==pr_year){
-	                if(today_month<pr_month)answer.add(count);
-	                else if(today_month==pr_month){
-	                    if(today_day<pr_day) answer.add(count);
-	                }
-	            }
-	            count++;
-	        }
-	        int[] arr= new int[answer.size()];
-	        for(int i=0;i<answer.size();i++) {
-	        	arr[i]=answer.get(i);
-	        }
-	        return arr;
-	    }
 	 
-	 public static void main(String args[]) {
-		 String a="2025.12.01";
-		 String[] b = a.split("\\.");
-		 for(String c:b) {
-			 System.out.println(c);
+	public static int[][] BuildMap(BufferedReader br, int n, int m) throws IOException{
+		int[][] map = new int[n][m];
+			 
+		for(int i=0;i<n;i++) {
+			 String line = br.readLine();
+			 map[i] = line.chars().map(c -> c-'0').toArray();
 		 }
-
+			 
+		return map;
+	}
+	
+	
+	public static int Solve(int[][] map){
+		int n = map.length;
+		int m = map[0].length;
+		
+		int[][] visited = new int[n][m];
+		
+		Queue<int[]> qu = new LinkedList<>();
+		int[] dx = {-1,0,1,0};
+		int[] dy = {0,1,0,-1};
 		 
+		qu.add(new int[] {0,0});
+		visited[0][0] = 1;
+		
+		while(!qu.isEmpty()) {
+			 int[] q = qu.poll();
+			 int x = q[0];
+			 int y = q[1];
+			 for(int j=0;j<4;j++) {
+				 int nx = x+dx[j];
+				 int ny = y+dy[j];				 
+				 if(nx<0 || nx >= n || ny < 0 || ny >= m || map[nx][ny]==0 || visited[nx][ny]!=0) continue;
+				 qu.add(new int[] {nx,ny});
+				 visited[nx][ny]=visited[x][y]+1;
+				 
+			 }
+		 }
+		
+		
+		return visited[n-1][m-1]; 
+	}
+	
+	 public static void main(String args[]) throws IOException{
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		 int[] nm = Arrays.stream(br.readLine().split(" ")).
+				 mapToInt(Integer::parseInt).
+				 toArray();
+		 int[][] map = BuildMap(br,nm[0],nm[1]);
+		 
+		 
+		 br.close();
+		 bw.write(Solve(map)+"\n");
+		 bw.flush();
+		 bw.close();
 	 }
 }
